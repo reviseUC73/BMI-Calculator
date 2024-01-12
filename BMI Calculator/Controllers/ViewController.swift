bf797c8fc194ac0b5580cd8fe9f7fabd1bc01af6
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-12 22:03:40 +0700

    first init code

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by ReviseUC73 on 12/1/2567 BE.
+//  Copyright © 2567 BE Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMI Calculator/Models/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  BMI Calculator
+//
+//  Created by ReviseUC73 on 12/1/2567 BE.
+//  Copyright © 2567 BE Angela Yu. All rights reserved.
+//
+
+import Foundation
